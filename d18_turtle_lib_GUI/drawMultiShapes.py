09da04d3790f5199192f6688bc066fd7fc8b7ac7
herman <herman.teng@outlook.com>
2021-09-12 12:33:33 -0400

    1. Rename Day 16 2. Day 18 turtle module and graphical user interface (GUI)

---
+++ d18_turtle_lib_GUI/drawMultiShapes.py
@@ -0,0 +1,25 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_n in range(3, 10):
+    tim.color(random_color())
+    draw_shape(shape_side_n)
